6241c62e331b734ba49b788414af6837d1d846d5
Ilia Perfilov <ind.climb@gmail.com>
2023-05-29 16:53:04 +0300

    models.py was updated

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,29 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return f"{self.name}, {self.description}"
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    preview = models.ImageField(upload_to='preview', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='Категория', **NULLABLE)
+    price = models.FloatField(verbose_name='Цена')
+    data_creating = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    data_updating = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения', **NULLABLE)
+
+    def __str__(self):
+        return f"{self.name}, {self.category}"
