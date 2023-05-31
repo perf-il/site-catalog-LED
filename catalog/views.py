b1d1d8dbfbb91512cf7e12990697ac0a5c0edaf2
Ilia Perfilov <ind.climb@gmail.com>
2023-05-31 13:49:33 +0300

    views.py was updated

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog\\home.html')
+    product_list = Product.objects.all()
+    context = {
+        'product_list': product_list
+    }
+    return render(request, 'catalog\\home.html', context)
 
 
 def contacts(request):
