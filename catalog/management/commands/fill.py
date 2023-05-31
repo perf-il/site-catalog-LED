8d5e6badcc5d870cc869d76c32690f0c653c745d
Ilia Perfilov <ind.climb@gmail.com>
2023-05-31 13:47:48 +0300

    added new command

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,6 +1,8 @@
+import json
+
 from django.core.management import BaseCommand
+from catalog.models import Product, Category
 import psycopg2
-import os
 
 
 class Command(BaseCommand):
@@ -14,9 +16,24 @@ class Command(BaseCommand):
     def handle(self, conn=connect, *args, **options):
         with conn:
             with conn.cursor() as cur:
-                cur.execute(f"TRUNCATE catalog_product;"
-                            f"TRUNCATE catalog_category CASCADE;\n")
+                cur.execute(f"TRUNCATE catalog_product RESTART IDENTITY;"
+                            f"TRUNCATE catalog_category RESTART IDENTITY CASCADE;\n")
             print('Tables catalog_product and catalog_category were cleared')
-        os.system('python manage.py loaddata .\catalog\data\catalog_data.json')
-        os.system('python manage.py loaddata .\catalog\data\product_data.json')
+
+        with open('.\catalog\data\catalog_data.json') as f:
+            data_category = json.load(f)
+        category_list = []
+        for table in data_category:
+            category_list.append(Category(**table.get('fields')))
+
+        Category.objects.bulk_create(category_list)
+
+        with open('.\catalog\data\product_data.json') as f:
+            data_product = json.load(f)
+        product_list = []
+        for product in data_product:
+            product_list.append(Product(**product.get('fields')))
+
+        Product.objects.bulk_create(product_list)
+
         print('Loading successfully')
