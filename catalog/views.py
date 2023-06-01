00df660ea41d1f906b7ae04423f8fdacbc44c27d
Ilia Perfilov <ind.climb@gmail.com>
2023-06-01 16:38:30 +0300

    new view for product_card.html was added

--- catalog/views.py
+++ catalog/views.py
@@ -6,14 +6,29 @@ from catalog.models import Product
 def home(request):
     product_list = Product.objects.all()
     context = {
-        'product_list': product_list
+        'product_list': product_list,
+        'title': 'Каталог'
     }
     return render(request, 'catalog\\home.html', context)
 
 
 def contacts(request):
-    return render(request, 'catalog\\contacts.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} ({phone}): {message}')
 
+    context = {
+        'title': 'Контакты'
+    }
 
-def about_me(request):
-    return render(request, 'catalog\\about_me.html')
+    return render(request, 'catalog\\contacts.html', context)
+
+def product_card(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {
+        'object': product,
+        'title': product.name
+    }
+    return render(request, 'catalog\\product_card.html', context)
