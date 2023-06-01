a4640066d557d58383dff0ce18f346818296c08f
Ilia Perfilov <ind.climb@gmail.com>
2023-06-01 16:37:29 +0300

    new url for card product was added

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from catalog.views import home, contacts, about_me
+from catalog.views import home, contacts, product_card
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts),
-    path('about/', about_me),
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contact'),
+    path('product/<int:product_id>/', product_card, name='product'),
 
 ]
