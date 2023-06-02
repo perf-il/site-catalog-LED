2845104211c85661cb6a88cec92fe44434664363
Ilia Perfilov <ind.climb@gmail.com>
2023-06-02 14:03:18 +0300

    fill.py was updated

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -2,27 +2,16 @@ import json
 
 from django.core.management import BaseCommand
 from catalog.models import Product, Category
-import psycopg2
 
 
 class Command(BaseCommand):
-    connect = psycopg2.connect(
-        host="localhost",
-        database="catalog",
-        user="postgres",
-        password="4444"
-    )
 
-    def handle(self, conn=connect, *args, **options):
-
+    def handle(self, *args, **options):
+        #удаление объектов из моделей
         Category.objects.all().delete()
         Product.objects.all().delete()
-        # with conn:
-        #     with conn.cursor() as cur:
-        #         cur.execute(f"TRUNCATE catalog_product RESTART IDENTITY;"
-        #                     f"TRUNCATE catalog_category RESTART IDENTITY CASCADE;\n")
-        #     print('Tables catalog_product and catalog_category were cleared')
 
+        #загрузка данных из файла для модели Catalog
         with open('.\catalog\data\catalog_data.json') as f:
             data_category = json.load(f)
         category_list = []
@@ -36,8 +25,10 @@ class Command(BaseCommand):
                                           description=description,
                                           pk=category_id))
 
+        #создание новых объектов в модели Catalog
         Category.objects.bulk_create(category_list)
 
+        #загрузка данных из файла для модели Product
         with open('.\catalog\data\product_data.json') as f:
             data_product = json.load(f)
         product_list = []
@@ -62,6 +53,7 @@ class Command(BaseCommand):
                                         data_updating=data_updating,
                                         pk=product_id))
 
+        #создание новых объектов в модели Product
         Product.objects.bulk_create(product_list)
 
         print('Loading successfully')
