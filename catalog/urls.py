41d459827eef452d6d266e9b1799b1e52aff2932
Ilia Perfilov <ind.climb@gmail.com>
2023-06-09 15:53:24 +0300

    new urls were added

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from catalog.views import home, contacts, product_card
+from catalog.views import home, contacts, CatalogDetailView, CatalogListView, BlogDetailView, BlogView, BlogUpdateView, \
+    BlogCreateView, BlogDeleteView
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
@@ -8,6 +9,12 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contact'),
-    path('product/<int:product_id>/', product_card, name='product'),
+    path('catalog/', CatalogListView.as_view(), name='catalog'),
+    path('product/<int:pk>/', CatalogDetailView.as_view(), name='product'),
+    path('blog/', BlogView.as_view(), name='blog'),
+    path('blog/create/', BlogCreateView.as_view(), name='blog_create'),
+    path('blog/<str:slug>/', BlogDetailView.as_view(), name='blog_article',),
+    path('blog/update/<str:slug>/', BlogUpdateView.as_view(), name='blog_update',),
+    path('blog/delete/<str:slug>/', BlogDeleteView.as_view(), name='blog_delete',),
 
 ]
