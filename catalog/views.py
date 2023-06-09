23d7cd6a0546b2d30358e2c340bb3df5a5c5e461
Ilia Perfilov <ind.climb@gmail.com>
2023-06-09 15:53:48 +0300

    new classes for CRUD were added

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-from catalog.models import Product
+from pytils.translit import slugify
+
+
+from catalog.models import Product, Blog
 
 
 def home(request):
@@ -10,6 +15,13 @@ def home(request):
         'title': 'Каталог'
     }
     return render(request, 'catalog/home.html', context)
+
+
+class CatalogListView(generic.ListView):
+    model = Product
+    extra_context = {
+        'title': 'Каталог'
+    }
 
 
 def contacts(request):
@@ -26,10 +38,65 @@ def contacts(request):
     return render(request, 'catalog/contacts.html', context)
 
 
-def product_card(request, product_id):
-    product = Product.objects.get(id=product_id)
-    context = {
-        'object': product,
-        'title': product.name
+class CatalogDetailView(generic.DetailView):
+    model = Product
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = self.get_object()
+        return context_data
+
+
+class BlogView(generic.ListView):
+    model = Blog
+    extra_context = {
+        'title': 'Блог'
     }
-    return render(request, 'catalog/product_card.html', context)
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        queryset = queryset.order_by('-data_creating')
+        return queryset
+
+
+class BlogDetailView(generic.DetailView):
+    model = Blog
+
+    def get_object(self, queryset=None):
+        post = super().get_object()
+        post.add_view()
+        post.save()
+        return post
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = self.get_object()
+        return context_data
+
+
+class BlogCreateView(generic.CreateView):
+    model = Blog
+    fields = ['title_name', 'content', 'preview']
+    success_url = reverse_lazy('catalog:blog')
+
+    def form_valid(self, form):
+        if form.is_valid:
+            fields = form.save(commit=False)
+            fields.slug = slugify(form.cleaned_data['title_name'])
+            fields.save()
+        return super().form_valid(form)
+
+
+class BlogUpdateView(generic.UpdateView):
+    model = Blog
+    fields = ['title_name', 'content', 'preview']
+    success_url = reverse_lazy('catalog:blog')
+
+
+class BlogDeleteView(generic.DeleteView):
+    model = Blog
+    success_url = reverse_lazy('catalog:blog')
+
+
+
