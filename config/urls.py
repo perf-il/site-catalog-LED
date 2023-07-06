370394f82c0a8c09cc3106d1d518d088c7b8a2fd
Ilia Perfilov <ind.climb@gmail.com>
2023-07-06 18:09:48 +0300

    path for new app was added

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
+    path('users', include('users.urls', namespace='users')),
 
 ]
 
